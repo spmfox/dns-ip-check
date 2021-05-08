a253b489204df455294edb350f977004412b08f1
spmfox <spmfox@foxwd.com>
2021-05-08 13:08:56 -0400

    Added back email function.

--- dns-ip-check.sh
+++ dns-ip-check.sh
@@ -12,12 +12,18 @@
 opt_FriendlyName=""                             # This name will appear in the message
 opt_TelegramBotToken=""                         # Token of Telegram Bot
 opt_TelegramMessageID=""                        # ID of the Telegram conversation to post the alert to
+opt_EmailServer=""				# Email SMTP server
+opt_EmailFrom=""				# Email to send from
+opt_EmailTo=""					# Email to send to
+opt_EmailUser=""				# Username for email
+opt_EmailPassword=""				# Password for email
 opt_AlertTimeoutSeconds="43200"                 # Alerts wont go out within these seconds of the last alert
 opt_DNSserver=""                                # Server to check DNS records, use the web server most likely
 opt_DNSname=""                                  # (sub)domain to check the IP address for
 opt_IPcheckServer="https://icanhazip.com"	# Service for checking IP address, should return ONLY the IP
 
 file_AlertFile="/dev/shm/.dns-ip-check.txt"	# Full path to the file where the last message is stored
+file_AlertEmail="/dev/shm/.dns-ip-check.eml"	# Full path to the file where the email will be temporarily stored
 
 str_DNSreply=$(nslookup $opt_DNSname $opt_DNSserver |grep "Address:" |grep -v '#53' |awk '{print $2}')
 str_IPreply=$(curl -s $opt_IPcheckServer)
@@ -31,7 +37,21 @@ EOF
 )
 
 function fn_TriggerMessage {
-str_CurlTelegram=$(curl -s -X POST -H "Content-Type: application/json" -d "$str_GenerateJSON" https://api.telegram.org/bot$opt_TelegramBotToken/sendMessage)
+ echo $str_FailureMessage > $file_AlertFile
+ if [ -n "$opt_TelegramBotToken" ] && [ -n "$opt_TelegramMessageID" ]; then
+  str_CurlTelegram=$(curl -s -X POST -H "Content-Type: application/json" -d "$str_GenerateJSON" https://api.telegram.org/bot$opt_TelegramBotToken/sendMessage)
+ else
+  echo "One or both Telegram settings are missing, not sending Telegram message."
+ fi
+ if [ -n "$opt_EmailServer" ] && [ -n "$opt_EmailFrom" ] && [ -n "$opt_EmailTo" ] && [ -n "$opt_EmailUser" ] && [ -n "$opt_EmailPassword" ]; then
+  echo "From: $opt_EmailFrom" > $file_AlertEmail
+  echo "To: $opt_EmailTo" >> $file_AlertEmail
+  echo "Subject: $opt_FriendlyName DNS Check Alert" >> $file_AlertEmail
+  str_CurlEmail=$(curl --ssl-reqd smtp://$opt_EmailServer --mail-from $opt_EmailFrom --mail-rcpt $opt_EmailTo --upload-file $file_AlertEmail --user "$opt_EmailUser":"$opt_EmailPassword"
+  rm $file_AlertEmail
+ else
+  echo "One or more email settings are missing, not sending email message."
+ fi
 }
 
 if [ "$str_DNSreply" == "$str_IPreply" ]; then
@@ -50,6 +70,5 @@ if [ -e $file_AlertFile ]; then
  fi
 else
  echo $str_FailureMessage
- echo $str_FailureMessage > $file_AlertFile
  fn_TriggerMessage
 fi
