3b7444798df54463c5f76d32889609d93555509e
spmfox <spmfox@foxwd.com>
2021-05-08 12:41:19 -0400

    Fixed variable names.

--- dns-ip-check.sh
+++ dns-ip-check.sh
@@ -18,11 +18,11 @@ opt_IPcheckServer="https://icanhazip.com"	# Service for checking IP address, sho
 
 file_AlertFile="/dev/shm/.dns-ip-check.txt"	# Full path to the file where the last message is stored
 
-var_DNSreply=$(nslookup $opt_DNSname $opt_DNSserver |grep "Address:" |grep -v '#53' |awk '{print $2}')
-var_IPreply=$(curl -s $opt_IPcheckServer)
+str_DNSreply=$(nslookup $opt_DNSname $opt_DNSserver |grep "Address:" |grep -v '#53' |awk '{print $2}')
+str_IPreply=$(curl -s $opt_IPcheckServer)
 
-str_FailureMessage="dns-ip-check.sh: $opt_FriendlyName IP does NOT match DNS entry. $opt_DNSname is: ($var_DNSreply). $opt_IPcheckServer returns ($var_IPreply) as public IP."
-str_SuccessMessage="dns-ip-check.sh: $opt_FriendlyName IP matches DNS entry. $opt_DNSname resolves to $var_DNSreply. $opt_IPcheckServer returns $var_IPreply as public IP."
+str_FailureMessage="dns-ip-check.sh: $opt_FriendlyName IP does NOT match DNS entry. $opt_DNSname is: ($str_DNSreply). $opt_IPcheckServer returns ($str_IPreply) as public IP."
+str_SuccessMessage="dns-ip-check.sh: $opt_FriendlyName IP matches DNS entry. $opt_DNSname resolves to $str_DNSreply. $opt_IPcheckServer returns $str_IPreply as public IP."
 
 str_GenerateJSON=$(cat <<EOF 
 {"chat_id": "$opt_TelegramMessageID", "text": "$str_FailureMessage"}
@@ -33,7 +33,7 @@ function fn_TriggerMessage {
 str_CurlTelegram=$(curl -s -X POST -H "Content-Type: application/json" -d "$str_GenerateJSON" https://api.telegram.org/bot$opt_TelegramBotToken/sendMessage)
 }
 
-if [ "$var_DNSreply" == "$var_IPreply" ]; then
+if [ "$str_DNSreply" == "$str_IPreply" ]; then
  echo $str_SuccessMessage
 fi
 
